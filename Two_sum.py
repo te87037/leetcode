8feb84fe17294176cc96d736e8742c8b7efb19c4
te87037 <te87037@gmail.com>
2017-05-04 14:36:30 +0800

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dictx = {}
+        for i,n in enumerate(nums,0):
+            rest = target - n
+            if rest in dictx:
+                return dictx[rest],i
+            dictx[n] = i
